c3195d2ab82e884e214325d274eda77f3b8e502a
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-05-10 14:28:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// To get more acquainted with `while`, your task is to write a function,
+// `countDown`, that takes in any positive integer and, starting from that number,
+// counts down to zero using `console.log()`. So, when written if you were to run:
+function countDown(m){
+  let i = m;
+  while (i>=0) {
+    console.log(i)
+    i--
+  }
+  }
+  countDown(10)
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countdown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
+} 
