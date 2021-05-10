d7e516887a1a457d50241b6d76b35aafc56698f8
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-05-10 14:28:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function countDown(m){
   }
   }
   countDown(10)
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countdown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
+} 
