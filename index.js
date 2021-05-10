cbcd8975c79e9caa36fd4ffb4f7c64fe19a99034
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-05-10 14:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+// To get more acquainted with `while`, your task is to write a function,
+// `countDown`, that takes in any positive integer and, starting from that number,
+// counts down to zero using `console.log()`. So, when written if you were to run:
+function countDown(m){
+  let i = m;
+  while (i>=0) {
+    console.log(i)
+    i--
+  }
+  }
+  countDown(10)
